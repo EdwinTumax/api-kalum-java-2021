5591a06035174be9c83a3a5c40c4a24483254b8b
Edwin Tumax <edwintumax@gmail.com>
2021-08-20 23:39:37 -0600

    Delete src directory

--- src/main/java/edu/kalum/notas/core/models/dao/IAsignacionAlumnoDao.java
+++
@@ -1,8 +0,0 @@
-package edu.kalum.notas.core.models.dao;
-
-import edu.kalum.notas.core.models.entities.AsignacionAlumno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IAsignacionAlumnoDao extends JpaRepository<AsignacionAlumno, String> {
-
-}
--- src/main/java/edu/kalum/notas/core/models/dao/IClaseDao.java
+++
@@ -1,8 +0,0 @@
-package edu.kalum.notas.core.models.dao;
-
-import edu.kalum.notas.core.models.entities.Clase;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IClaseDao extends JpaRepository<Clase, String> {
-
-}
