2c45d0266196106e3d71f657266af54965d7e119
Edwin Tumax <edwintumax@gmail.com>
2021-08-20 23:44:16 -0600

    update branch master

---
+++ src/main/java/edu/kalum/notas/core/KalumApplication.java
@@ -0,0 +1,13 @@
+package edu.kalum.notas.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalumApplication.class, args);
+	}
+
+}
